9ddf8114d488b33b718df1bced457c65519f973d
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-10-08 15:18:33 +0900

    10/8 practice

--- main.c
+++ main.c
@@ -4,22 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	//practice03 
-	int input;
 	
-	printf("input a number : ");
-	scanf("%d", &input);
+	int i=0, meter;
 	
-	if(input>0)
-	
-		printf("positive\n");
+	while (i<3)
+	{
+		meter=i*1609;
+		printf("%d 마일은%d미터입니다.\n", i, meter);
+		i++;
+	}
 
 
-	else if(input<0)
-		printf("negative\n");
-		
-	else 
-		printf("zero\n");
 		
 	return 0;
 }
