90e72e038ff63a14a3a6e0d4c4ccf36881d48eef
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-10-08 15:31:57 +0900

    10/8 practice

--- main.c
+++ main.c
@@ -5,15 +5,9 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i=0, meter;
-	
-	while (i<3)
-	{
-		meter=i*1609;
-		printf("%d 마일은%d미터입니다.\n", i, meter);
-		i++;
-	}
-
+	int i;
+	for(i=0;i<10;i++)
+		printf("Hello World\n");
 
 		
 	return 0;
